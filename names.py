a8f477c35b5168987824cc2229547c1d3476e129
High Performance File Systems <hpfs-admin@iu.edu>
2018-07-18 14:12:41 -0400

    adding support for dataset_id tracking & move to in-mem db for test

--- names.py
+++ names.py
@@ -6,10 +6,13 @@ import stat
 def create_names_table(conn):
     conn.execute('''
         create table name (
-          name_id    integer not null primary key,
+          name_id    integer primary key autoincrement ,
+          dataset_id integer not null,
           fid        text,
           name       text,
-          parent_fid text)''')
+          parent_fid text,
+          unique (name_id, dataset_id)
+          )''')
     conn.execute('create index name_np on name (name_id, parent_fid)')
     conn.execute('create index name_f on name (fid)')
     conn.execute('create index name_p on name (parent_fid)')
@@ -19,19 +22,19 @@ def drop_names_table(conn):
     conn.execute("drop table name")
 
 
-def insert_name(curs, fid, name, parent_fid):
-    curs.execute("insert into name (fid, name, parent_fid) values (?, ?, ?)",
-                 [fid, name, parent_fid]).fetchone()
+def insert_name(curs, dataset_id, fid, name, parent_fid):
+    curs.execute("insert into name (dataset_id, fid, name, parent_fid) values (?, ?, ?, ?)",
+                 [dataset_id, fid, name, parent_fid]).fetchone()
 
 
-def populate_names(curs, parent_dir, parent_id):
+def populate_names(curs, dataset_id, parent_dir, parent_id):
     for entry in os.listdir(parent_dir):
         path_entry = os.path.join(parent_dir, entry)
         try:
             st = os.stat(path_entry)
-            insert_name(curs, st.st_ino, entry, parent_id)
+            insert_name(curs, dataset_id, st.st_ino, entry, parent_id)
             if stat.S_ISDIR(st.st_mode):
-                populate_names(curs, path_entry, st.st_ino)
+                populate_names(curs, dataset_id, path_entry, st.st_ino)
         except OSError:
             pass
 
@@ -39,7 +42,7 @@ def populate_names(curs, parent_dir, parent_id):
 def fid_to_path(conn, srch_fid):
     def helper(fid0, sofar):
         sql = "select * from name where fid = ?"
-        (_, fid, name, parent_fid) = conn.execute(sql, [fid0]).fetchone()
+        (_, _, fid, name, parent_fid) = conn.execute(sql, [fid0]).fetchone()
         sofar.append(name)
         if parent_fid is None:
             return sofar
@@ -60,18 +63,14 @@ def dump(cur):
 
 
 def test_names():
-    conn = sqlite3.connect("names.db")
-    drop_names_table(conn)
+    conn = sqlite3.connect(":memory:")
     create_names_table(conn)
     curs = conn.cursor()
-    populate_names(curs, "test_dir", None)
+    dataset_id = 1
+    populate_names(curs, dataset_id, "test_dir", None)
     conn.commit()
     sql = """select * from name where name like ? limit 100"""
     curs = conn.execute(sql, ["file2.txt"])
-    (_, fid, _, _) = curs.fetchone()
-    print(fid_to_path(conn, fid))
+    (_, _, fid, _, _) = curs.fetchone()
+    assert fid_to_path(conn, fid) == '/a/ab/file2.txt'
     conn.close()
-
-
-if __name__ == "__main__":
-    test_names()
