878b600a1fd212421a8738f20a48816ac79e906a
sdslavin <slavin@iu.edu>
2018-07-27 18:10:08 -0400

    Worked out fid_to_path() issues. Need a little more work on dealing with shared directory names (i.e. cut them out of the returned path).

--- names.py
+++ names.py
@@ -42,20 +42,62 @@ def populate_names(curs, parent_dir, parent_id):
 
 
 def fid_to_path(conn, srch_fid):
-    def helper(fid0, sofar):
-        sql = "select * from name where fid = ?"
-        (fid, name, parent_fid) = conn.execute(sql, [fid0]).fetchone()
-        sofar.append(name)
-        if parent_fid is None:
-            return sofar
+    def helper(cur, fid0, path_so_far):
+        sql = "select fid, name, parent_fid from name where fid = ?"
+        search_output = cur.execute(sql, [fid0]).fetchone()
+        # FWIW: [0x200000007:0x1:0x0] is the FID for the root of the lustre filesystem. We could iterate until
+        # we obtain that for the parent FID. However, the null search works as well, and will not cause an infinite
+        # loop if we have an incomplete path in the database.
+        #
+        # Sharded directories will have a filename that includes a FID with the []:x notation, where the FID is
+        # inside the [] brackets, as usual, and the :x extension with have x equal to the value of the MDT index
+        # on which that path is hung. Running 'lfs fid2path [0x240000401:0x5:0x0]:0' for example will point to the
+        # same directory name as [0x240000401:0x5:0x0], and to the name of the parent directory fid:
+        # >>> names.fid_to_path(db,'0x240000402:0x4e92:0x0')
+        # u'/striped/[0x240000401:0x5:0x0]:1/1GB.0'
+        #
+        # On the zester-mds01 client VM:
+        #
+        # [root@zester-mds01 ~]# lfs path2fid /mnt/zester/striped/*
+        # /mnt/zester/striped/1GB.0: [0x240000402:0x4e92:0x0]
+        # /mnt/zester/striped/1GB.1: [0x200000402:0x10a:0x0]
+        # /mnt/zester/striped/1GB.2: [0x240000402:0x4e97:0x0]
+        # /mnt/zester/striped/1GB.3: [0x200000402:0x10b:0x0]
+        #
+        # [root@zester-mds01 ~]# lfs path2fid /mnt/zester/striped
+        # [0x200000402:0x105:0x0]
+        #
+        # [root@zester-mds01 ~]# lfs fid2path zester [0x240000401:0x5:0x0]:0
+        # striped/
+        #
+        # [root@zester-mds01 ~]# lfs fid2path zester [0x240000401:0x5:0x0]:1
+        # striped/
+        #
+        # [root@zester-mds01 ~] lfs fid2path zester 0x200000402:0x105:0x0
+        # striped
+        #
+        # [root@zester-mds01 ~]# lfs path2fid --parents /mnt/zester/striped
+        # [0x200000007:0x1:0x0]/striped
+        # [root@zester-mds01 ~]# lfs fid2path zester 0x200000007:0x1:0x0
+        # /
+        #
+        if search_output is None:
+            # Need to figure out how to deal with cur.execute.fetchone() returning null results
+            return path_so_far
         else:
-            return helper(parent_fid, sofar)
+            (fid, name, parent_fid) = search_output
+            print(fid, name, parent_fid)
+            path_so_far.append(name)
+            return helper(cur, parent_fid, path_so_far)
 
-    ls0 = helper(srch_fid, [])
+    cur = conn.cursor()
+
+    ls0 = helper(cur, srch_fid, [])
     ls0.reverse()
     path_build = ""
     for name0 in ls0:
         path_build = path_build + "/" + name0
+    cur.close()
     return path_build
 
 
